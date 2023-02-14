1b4ee34752f61f4af4b9b9147e42b66d7e26fe42
RuLe0G <khlebnikov2015@yandex.ru>
2023-02-15 00:48:00 +0700

    New feature 15.02
    Vaulting, coyote time, jump buffer

--- Assets/ThridPersonController.cs
+++ Assets/ThridPersonController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -21,10 +22,24 @@ public class ThridPersonController : MonoBehaviour
     [SerializeField]
     private Camera playerCamera;
 
+    private int vaultLayer;
+    private float playerHeight;
+    private float playerRadius;
+
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
+
     private void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
         playerActionsAsset = new ThridPersonAsset();
+        vaultLayer = LayerMask.NameToLayer("VaultLayer");
+        vaultLayer = ~vaultLayer;
+        playerHeight = GetComponent<CapsuleCollider>().height;
+        playerRadius = GetComponent<CapsuleCollider>().radius;
     }
     private void OnEnable()
     {
@@ -32,10 +47,35 @@ public class ThridPersonController : MonoBehaviour
         move = playerActionsAsset.Player.Move;
         playerActionsAsset.Player.Enable();
     }
+
+
+
     private void OnDisable()
     {
         playerActionsAsset.Player.Jump.started -= DoJump;
         playerActionsAsset.Player.Disable();
+    }
+
+
+    private void Update()
+    {
+        if (playerActionsAsset.Player.Jump.triggered) 
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
     }
     private void FixedUpdate()
     {
@@ -81,16 +121,53 @@ public class ThridPersonController : MonoBehaviour
 
     private void DoJump(InputAction.CallbackContext obj)
     {
-        if (IsGrounded())
+        jumpBufferCounter = jumpBufferTime;
+        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
         {
             forceDirection += Vector3.up * jumpForce;
+            coyoteTimeCounter = 0f;
+            jumpBufferCounter= 0f;
         }
+        else
+        { 
+            Vault();
+        }
+    }
+    private void Vault()
+    {
+        Ray ray = new Ray(this.transform.position + Vector3.up * 0.25f, this.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit firstHit, 1f,vaultLayer))
+        {           
+            StartCoroutine(LerpVault(firstHit.point + (Vector3.up * playerHeight), 0.5f));
+        }
+    }
+    IEnumerator LerpVault(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = this.transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 pos = this.transform.position + Vector3.up * 0.25f;
+        Vector3 direction = this.transform.forward * 5f;
+        Gizmos.DrawRay(pos, direction);        
+        
     }
 
     private bool IsGrounded()
     {
-        Ray ray = new Ray(this.transform.position + Vector3.up * 0.25f, Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit, 2f))
+        Ray ray = new Ray(this.transform.position - Vector3.up * 0.25f, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, 1f))
             return true;
         else
             return false;
